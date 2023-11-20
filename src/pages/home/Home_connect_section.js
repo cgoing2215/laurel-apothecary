8d21f5c24f1ced52a518abe9c734064cb3cf2b4d
Caroline Going <carolinegoing2@gmail.com>
2023-11-20 15:33:59 -0500

    updated connect section background image and css styling, added images to assets folder for offering tiles

--- src/pages/home/Home_connect_section.js
+++ src/pages/home/Home_connect_section.js
@@ -2,7 +2,20 @@ import React from 'react'
 
 function Homepage_connect_section() {
   return (
-    <div>Homepage_connect_section</div>
+    <div className='homepage-connect-container'>
+      <h1 className='homepage-connect-headline'>
+        Connect
+      </h1>
+      <div className='homepage-connect-image-container'>
+        <h3>
+          If you are interested in working together on this journey, 
+          don’t hesitate to get in touch.
+        </h3>
+        <button>
+          CONTACT ME
+        </button>
+      </div>
+    </div>
   )
 }
 
