9e17856dbecdce57ccd800a1e80bc89836e3593b
Caroline Going <carolinegoing2@gmail.com>
2023-11-21 18:24:14 -0500

    Added social icons to footer and updated css, cleaned up spacing and font size for homepage about section

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import instagramIcon from './icons/Instagram-icon.png'
+import pinterestIcon from './icons/Pinterest-icon.png'
+import facebookIcon from './icons/Facebook-icon.png'
 import "./Footer.css"
 
 function Footer() {
   return (
-    <div className='footer-container'>Footer</div>
+    <div className='footer-container'>
+      <div className='social-icons'>
+        <img src={instagramIcon} alt='instagram icon'/>
+        <img src={pinterestIcon} alt='pinterest icon'/>
+        <img src={facebookIcon} alt='facebook icon'/>
+      </div>
+      <h4>Laurel Apothecary</h4>
+    </div>
   )
 }
 
